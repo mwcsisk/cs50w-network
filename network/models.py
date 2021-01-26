44ea9a184d9bfddf05714dade917421f3a6d87d9
Martin Sisk <mwcsisk@gmail.com>
2021-01-26 14:18:12 -0800

    Add managers to models

--- network/models.py
+++ network/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    users = models.Manager()
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', editable=False)
     body = models.CharField(max_length=240)
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="liked")
+
+    posts = models.Manager()
